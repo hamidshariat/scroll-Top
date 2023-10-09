62fe90b0c475edc77b1404737dfb51090c6e07ce
hamidshariat <hamid13841147@gmail.com>
2023-10-09 13:20:12 +0330

    Modify the code

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-document.addEventListener('DOMContentLoaded' , function() {
-    let preloader = document.getElementById('preloader')
-    let content = document.getElementById('content')
+document.addEventListener("DOMContentLoaded", function () {
+  let preloader = document.getElementById("preloader");
+  let content = document.getElementById("content");
 
-    preloader.style.display = 'none';
-    content.style.display = 'block'
+  preloader.style.display = "none";
+  content.style.display = "block";
 });
 
-const backToTop = document.getElementById('backToTop');
-backToTop.addEventListener('click' , function(e) {
-    scrollTop(0 , 1000)
-})
+const backToTop = document.getElementById("backToTop");
+backToTop.addEventListener("click", function (e) {
+  scrollTop(0, 1000);
+});
 
-function scrollTop(scroll , duration) {
-    let doc = document.documentElement;
-    let currentTime = duration;
-    let speed = 10;
+function scrollTop(scroll, duration) {
+  let doc = document.documentElement;
+  let currentTime = duration;
+  let speed = 10;
 
-    let animate = () => {
-        if(currentTime < 0) return;
-        setTimeout(() => {
-            doc.scrollTop -= doc.scrollTop / (currentTime / speed)
-            console.log(doc.scrollTop)
-            currentTime -= speed;
-            animate()
-        }, speed);
-    }
-
-    animate();
+  let animate = () => {
+    if (currentTime < 0) return;
+    setTimeout(() => {
+      doc.scrollTop -= doc.scrollTop / (currentTime / speed);
+      currentTime -= speed;
+      animate();
+    }, speed);
+  };
+  animate();
 }
-
-window.addEventListener('scroll' , function(e) {
-    if(document.documentElement.scrollTop > 250) {
-        backToTop.style.display = 'flex';
-    } else {
-        backToTop.style.display = 'none'
-    }
-})
+window.addEventListener("scroll", function (e) {
+  if (document.documentElement.scrollTop > 250) {
+    backToTop.style.display = "flex";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
